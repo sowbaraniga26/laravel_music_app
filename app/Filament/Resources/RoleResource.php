787d6e8106f5c08e868c19be6b4976b070456a76
sowbaraniga26 <sowbaraniga26@gmail.com>
2024-11-22 19:41:55 +0530

    Role Resource added
    php artisan make:filament-resource Role

---
+++ app/Filament/Resources/RoleResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RoleResource\Pages;
+use App\Filament\Resources\RoleResource\RelationManagers;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use Spatie\Permission\Models\Role;
+
+use Filament\Tables\Columns\TextColumn;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Section;
+
+class RoleResource extends Resource
+{
+    protected static ?string $model = Role::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+               Section::make('User Roles')
+               ->description('Role Must be Minimum 2 Maximum 255 Character')
+               ->Schema([
+                 TextInput::make('name')
+                   ->minLength(2)
+                   ->maxLength(255)
+                   ->required()
+                   ->unique()
+               ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRoles::route('/'),
+            'create' => Pages\CreateRole::route('/create'),
+            'edit' => Pages\EditRole::route('/{record}/edit'),
+        ];
+    }
+}
