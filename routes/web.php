7e7c5e27dbdb80fbe65dc34fa596d8a09231f04c
sowbaraniga26 <sowbaraniga26@gmail.com>
2024-12-07 19:18:45 +0530

    MiddleGuest auth middleware added

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,21 @@ Route::get('/services', [HomeController::class, 'services'])->name('home.service
 Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
 
 
-Route::get('/login', [AuthController::class, 'login'])->name('home.login');
-Route::post('/authenticate', [AuthController::class, 'authenticate'])->name('home.authenticate');
-Route::get('/profile',[AuthController::class, 'profile'])->name('home.profile');
-Route::get('logout',[AuthController::class, 'logout'])->name('home.logout');
-Route::get('/register',[AuthController::class, 'register'])->name('home.register');
-Route::post('/store', [AuthController::class, 'store'])->name('home.store');
+// Route::get('/login', [AuthController::class, 'login'])->name('home.login')->middleware('guest');
+// Route::post('/authenticate', [AuthController::class, 'authenticate'])->name('home.authenticate');
+// Route::get('/profile',[AuthController::class, 'profile'])->name('home.profile')->middleware('auth');
+// Route::get('logout',[AuthController::class, 'logout'])->name('home.logout')->middleware('auth');
+// Route::get('/register',[AuthController::class, 'register'])->name('home.register')->middleware('guest');
+// Route::post('/store', [AuthController::class, 'store'])->name('home.store');
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('home.login');
+    Route::post('/authenticate', [AuthController::class, 'authenticate'])->name('home.authenticate');
+    Route::get('/register', [AuthController::class, 'register'])->name('home.register');
+    Route::post('/store', [AuthController::class, 'store'])->name('home.store');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [AuthController::class, 'profile'])->name('home.profile');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('home.logout');
+});
