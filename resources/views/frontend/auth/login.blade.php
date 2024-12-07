58cdf169a76357ea993c953c9e5f07b45fa43185
sowbaraniga26 <sowbaraniga26@gmail.com>
2024-12-07 19:33:04 +0530

    show and hide password Login UI added

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -15,10 +15,15 @@
             <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" required>
           </div>
 
-          <div class="mb-3">
+          <div class="mb-3 position-relative">
             <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password" required>
-          </div>
+            <div class="input-group">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password" required>
+                <button type="button" class="btn btn-outline-secondary toggle-password" tabindex="-1">
+                    <i class="fa fa-eye"></i>
+                </button>
+            </div>
+        </div>
 
           <div class="d-flex justify-content-between align-items-center mb-3">
             <div>
@@ -38,4 +43,21 @@
     </div>
   </div>
 
+@endsection
+
+@section('login_scripts')
+<script>
+    $(document).ready(function () {
+        console.log("login working");
+        
+        $('.toggle-password').on('click', function () {
+            const passwordField = $('#password');
+            const type = passwordField.attr('type') === 'password' ? 'text' : 'password';
+            passwordField.attr('type', type);
+
+            // Toggle the eye icon
+            $(this).find('i').toggleClass('fa-eye fa-eye-slash');
+        });
+    });
+</script>
 @endsection
